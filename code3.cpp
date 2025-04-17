24f0d60641679391b0b65da97c2946054efbde29
Rishab Raj <rr0948555@gmail.com>
2025-04-17 11:32:33 +0530

    Add files via upload

---
+++ code3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int marks[] = {23, 45, 56, 89};
+    int mathMarks[4];
+    mathMarks[0] = 234;
+    mathMarks[1] = 376;
+    mathMarks[2] = 456;
+    mathMarks[3] = 564;
+
+    cout<<"These are Math marks"<<endl;;
+    cout<<mathMarks[0]<<endl;
+    cout<<mathMarks[1]<<endl;
+    cout<<mathMarks[2]<<endl;
+    cout<<mathMarks[3]<<endl;
+
+     cout<<"These are marks"<<endl;;
+    cout<<marks[0]<<endl;
+    cout<<marks[1]<<endl;
+    marks[2] = 455;
+    cout<<marks[2]<<endl;
+    cout<<marks[3]<<endl;
+    return 0;
+}
