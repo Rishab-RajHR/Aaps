b2ff523713a8083ec79a69b3d41fa74779816861
Rishab Raj <rr0948555@gmail.com>
2025-04-17 11:30:55 +0530

    Add files via upload

---
+++ code2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //   without writing marks[4] C++ automatically understand 
+    int marks[] = {23, 45, 56, 89};
+    cout<<marks[0]<<endl;
+    cout<<marks[1]<<endl;
+    cout<<marks[2]<<endl;
+    cout<<marks[3]<<endl;
+    return 0;
+}
